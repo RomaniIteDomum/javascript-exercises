d66b8606ff52ec2dc5bbd4a22edbde5a798d048e
RomaniIteDomum <hakan-kilinc@web.de>
2023-10-11 16:05:27 +0200

    Implement leap year logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+ return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 };
 
+console.log(leapYears(2104));
 // Do not edit below this line
 module.exports = leapYears;
