312cbb405be802382f5d0bcd08ccdff7c449a6ce
RomaniIteDomum <hakan-kilinc@web.de>
2023-10-11 10:42:10 +0200

    Implement function that removes elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
-const removeFromArray = function(array, numberToRemove) {
-    let index = array.indexOf(numberToRemove);
-    array.splice(index, 1);
+// const removeFromArray = function(array, numberToRemove) {
+//     let index = array.indexOf(numberToRemove);
+//     array.splice(index, 1);
 
-    return array;
-};
+//     return array;
+// };
+
+// console.log(removeFromArray([1, 2, 3 ], 3));
+
+const removeFromArray = function (originalArray, ...numbers) {
+    return originalArray.filter((element) => !numbers.includes(element));
+
+} 
 
 console.log(removeFromArray([1, 2, 3 ], 3));
-
-
 
 // Do not edit below this line
 module.exports = removeFromArray;
